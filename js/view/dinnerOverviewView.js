95fc9e47b57154eedfb339577e9c7c47b6dd6510
Erica Bronge <erica.bronge@gmail.com>
2015-02-11 15:35:02 +0100

    Minor fixes in dinner overview view

--- js/view/dinnerOverviewView.js
+++ js/view/dinnerOverviewView.js
@@ -7,27 +7,28 @@ var DinnerOverviewView = function (container, model) {
 	    var dessert = model.getSelectedDish("dessert");
 	    
 	    if (typeof starter != 'undefined') {	    
-	        container.find("#chosen-starter-img").append("<img class='dishImages' src='images/" + starter.image + "'>");
-	        container.find("#chosen-starter").append(starter.name);
-	        container.find("#chosen-starter-price").append(model.getDishPrice(starter.id) + " SEK");
+	        container.find("#chosen-starter-img").html("<img class='dishImages' src='images/" + starter.image + "'>");
+	        container.find("#chosen-starter").html(starter.name);
+	        container.find("#chosen-starter-price").html(model.getDishPrice(starter.id) + " SEK");
 	    }
 	    
 	    if (typeof main != 'undefined') {	
-	        container.find("#chosen-main-img").append("<img class='dishImages' src='images/" + starter.image + "'>");
-	        container.find("#chosen-main").append(starter.name);
-	        container.find("#chosen-main-price").append(model.getDishPrice(starter.id) + " SEK");
+	        container.find("#chosen-main-img").html("<img class='dishImages' src='images/" + starter.image + "'>");
+	        container.find("#chosen-main").html(starter.name);
+	        container.find("#chosen-main-price").html(model.getDishPrice(starter.id) + " SEK");
 	    }
 	    if (typeof dessert != 'undefined') {	
-	        container.find("#chosen-dessert-img").append("<img class='dishImages' src='images/" + starter.image + "'>");
-	        container.find("#chosen-dessert").append(starter.name);
-	        container.find("#chosen-dessert-price").append(model.getDishPrice(starter.id) + " SEK");
+	        container.find("#chosen-dessert-img").html("<img class='dishImages' src='images/" + starter.image + "'>");
+	        container.find("#chosen-dessert").html(starter.name);
+	        container.find("#chosen-dessert-price").html(model.getDishPrice(starter.id) + " SEK");
 	    }
 	    
-	    container.find("#total-price-td").append(model.getTotalMenuPrice() + " SEK");
+	    container.find("#total-price-td").html(model.getTotalMenuPrice() + " SEK");
 	}
 	
 	this.show = function() {
-	    container.show();	    
+	    container.show();
+	    this.update();
 	}
 	
 	this.hide = function() {
