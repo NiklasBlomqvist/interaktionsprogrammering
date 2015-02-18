3a2c0408f7f96fda3c7f154b958e2a693a781f18
Erica Bronge <erica.bronge@gmail.com>
2015-02-18 16:10:10 +0100

    Rewrote overview and preparation views to better hide empty tables

--- js/view/dinnerOverviewView.js
+++ js/view/dinnerOverviewView.js
@@ -11,27 +11,57 @@ var DinnerOverviewView = function (container, model) {
 	    var dessert = model.getSelectedDish("dessert");
 	    
 	    if (typeof starter != 'undefined') {	    
-	        container.find("#chosen-starter-img").html("<img class='dishImages' src='images/" + starter.image + "'>");
-	        container.find("#chosen-starter").html(starter.name);
-	        container.find("#chosen-starter-price").html(model.getDishPrice(starter.id) + " SEK");
-	    } else {
-	        container.find("#firstDishContainer").hide();
+	        container.find("#firstDishContainer").html("<tr>" +
+							"<td id='chosen-starter-img' class='borderImageNoBottom'>" +
+							    "<img class='dishImages' src='images/" + starter.image + "'>" +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-starter' class='dishName borderImage'>" +
+							starter.name +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-starter-price' class='dishPrice'>" +
+							    model.getDishPrice(starter.id) + " SEK" +
+							"</td>" +
+						"</tr>");
 	    }
 	    
-	    if (typeof main != 'undefined') {	
-	        container.find("#chosen-main-img").html("<img class='dishImages' src='images/" + main.image + "'>");
-	        container.find("#chosen-main").html(main.name);
-	        container.find("#chosen-main-price").html(model.getDishPrice(main.id) + " SEK");
-	    } else {
-	        container.find("#secondDishContainer").hide();
+	    if (typeof main != 'undefined') {	    
+	        container.find("#secondDishContainer").html("<tr>" +
+							"<td id='chosen-main-img' class='borderImageNoBottom'>" +
+							    "<img class='dishImages' src='images/" + main.image + "'>" +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-main' class='dishName borderImage'>" +
+							main.name +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-main-price' class='dishPrice'>" +
+							    model.getDishPrice(main.id) + " SEK" +
+							"</td>" +
+						"</tr>");
 	    }
 	    
-	    if (typeof dessert != 'undefined') {	
-	        container.find("#chosen-dessert-img").html("<img class='dishImages' src='images/" + dessert.image + "'>");
-	        container.find("#chosen-dessert").html(dessert.name);
-	        container.find("#chosen-dessert-price").html(model.getDishPrice(dessert.id) + " SEK");
-	    } else {
-	        container.find("#thirdDishContainer").hide();
+	    if (typeof dessert != 'undefined') {	    
+	        container.find("#thirdDishContainer").html("<tr>" +
+							"<td id='chosen-dessert-img' class='borderImageNoBottom'>" +
+							    "<img class='dishImages' src='images/" + dessert.image + "'>" +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-dessert' class='dishName borderImage'>" +
+							dessert.name +
+							"</td>" +
+						"</tr>" +
+						"<tr>" +
+							"<td id='chosen-dessert-price' class='dishPrice'>" +
+							    model.getDishPrice(dessert.id) + " SEK" +
+							"</td>" +
+						"</tr>");
 	    }
 	    
 	    container.find("#total-price-td").html(model.getTotalMenuPrice() + " SEK");
